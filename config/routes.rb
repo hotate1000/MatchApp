d58b81be976f97f084be88e67ad3ca0e57d1a063
hotate1000 <ooo.336.sss@gmail.com>
2020-09-14 00:16:03 +0900

    トップページにユーザー情報反映、usersコントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'posts#index';
+  root to: 'users#index';
   resources :posts, only: [:index];
+  resources :users, only: [:index, :show]
+  # get 'users/index'
+  # get 'users/show'
 end
