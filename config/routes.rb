25912adb7477ad6134e8d52b576e9c606d6ff265
hotate1000 <ooo.336.sss@gmail.com>
2020-09-14 17:02:55 +0900

    ログイン時の表示画面設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
+
+# routes.rbでrootパスの先を設定する
+# 今回はusers,controllersで指定
+  # devise_scope :user do
+  #   authenticated :user do
+  #     root to: 'users#index', as: :authenticated_root
+  #   end
+  #   unauthenticated :user do
+  #     root to: "devise/sessions#new", as: :unauthenticated_root
+  #   end
+  # end
   root to: 'users#index';
   resources :posts, only: [:index];
   resources :users, only: [:index, :show]
-  # get 'users/index'
-  # get 'users/show'
 end
