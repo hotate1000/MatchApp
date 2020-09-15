bc9044f582eb7f39f1c442002cf783e422229b1d
hotate1000 <ooo.336.sss@gmail.com>
2020-09-15 15:39:28 +0900

    仮登録

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,7 +1,11 @@
 class Relationship < ApplicationRecord
 
+  # class_name: "モデル"（モデル指定）
   belongs_to :follower, class_name: "User"
   belongs_to :following, class_name: "User"
+
+# follower_idフォローされているユーザー、followingフォローしているユーザー
+  # presenceから文字×
   validates :follower_id, presence: true
   validates :following_id, presence: true
 end
